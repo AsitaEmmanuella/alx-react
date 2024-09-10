b83fb9788b84a887f91aa2360e2676c775ec2c04
AsitaEmmanuella <asitaemmanuella19@gmail.com>
2024-09-10 22:10:55 +0100

    react immutable

---
+++ 0x06-React_Immutable/eslint.config.mjs
@@ -0,0 +1,12 @@
+
+// eslint.config.js
+export default [
+    {
+      files: ["**/*.js"],
+      rules: {
+        "no-multiple-empty-lines": ["error", { max: 0 }],
+        // Add other rules as needed
+      },
+    },
+  ];
+  
